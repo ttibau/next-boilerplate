2b305028212c51a375a0bacd8104aa120efcae5d
Thiago Tibau <ttibaudev@gmail.com>
2022-08-10 01:12:46 -0300

    Creating organizational form

--- components/Forms/Organizational/index.tsx
+++ components/Forms/Organizational/index.tsx
@@ -1,5 +1,5 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { Process } from '../../../types/entities/Process';
+import { IOrganizational } from '../../../types/entities/Organizational';
 import { Input } from '../../Input/styles';
 import Select from '../../Select';
 import { TextArea } from '../../Textarea';
@@ -9,48 +9,35 @@ const OrganizationalForm = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
     control,
-  } = useForm<Process>();
+  } = useForm<IOrganizational>();
 
-  const onSubmit: SubmitHandler<Process> = (data) => {
+  const onSubmit: SubmitHandler<IOrganizational> = (data) => {
     console.log(data);
   };
 
-  const macroProcessList = [
+  const parentList = [
     {
       value: '1',
-      label: 'Processo 1',
+      label: 'Presidencia',
     },
     {
       value: '2',
-      label: 'Processo 2',
-    },
-    {
-      value: '3',
-      label: 'Processo 3',
+      label: 'Diretoria 2',
     },
   ];
 
-  console.log(watch('name'));
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Styled.FormLabel htmlFor='name'>Name:</Styled.FormLabel>
-      <Input {...register('name', { required: true })} />
-      {errors.name && <span>This field is required</span>}
+      <Input {...register('label', { required: true })} />
+      {errors.label && <span>This field is required</span>}
       <Styled.Spacer />
-      <Styled.FormLabel htmlFor='macroProcess'>MacroProcess:</Styled.FormLabel>
-      <Select
-        options={macroProcessList}
-        control={control as any}
-        name='macroProcess'
-      />
-      {errors.macroProcess && <span>This field is required</span>}
+      <Styled.FormLabel htmlFor='parent'>Parent:</Styled.FormLabel>
+      <Select options={parentList} control={control as any} name='parent' />
+      {errors.parentId && <span>This field is required</span>}
       <Styled.Spacer />
-      <Styled.FormLabel htmlFor='target'>Target:</Styled.FormLabel>
-      <TextArea name='target' control={control as any} rows={3} />
-      {errors.target && <span>This field is required</span>}
     </form>
   );
 };
