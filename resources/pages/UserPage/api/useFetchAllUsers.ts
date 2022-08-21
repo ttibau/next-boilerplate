730e987d41f2bdcadb638575da8af0b31d93f53d
Thiago Tibau <ttibaudev@gmail.com>
2022-08-21 01:11:50 -0300

    using http module instead axios

--- resources/pages/UserPage/api/useFetchAllUsers.ts
+++ resources/pages/UserPage/api/useFetchAllUsers.ts
@@ -2,10 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import { IUser } from '../../../../types/entities/User';
 import { CustomError } from '../../../../types/general/CustomError';
 import { http } from '../../../../api/http';
-import axios from 'axios';
 
 const fetchAllUsers = async (): Promise<any> => {
-  const { data } = await axios.get('http://localhost:8080/api/usuarios');
+  const { data } = await http.get('http://localhost:8080/api/usuarios');
   return data;
 };
 
